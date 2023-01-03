8a4185d5625f8c43221ad556bdec7480a3061d55
Mr. Kiedes <akiedes@vtsd.com>
2023-01-03 06:56:24 -0500

    Initial Upload

---
+++ Lab11avst.java
@@ -0,0 +1,38 @@
+// Lab11avst.java
+// The "Sieve of Eratosthenes" Program
+// This is the student, starting version of the Lab11a assignment.
+
+
+import java.util.Scanner;
+
+
+public class Lab11avst
+{
+    public static void main(String[] args)
+    {
+        // This main method needs additions for the 100 point version.
+        Scanner input = new Scanner(System.in);
+        final int MAX = 100;
+        boolean primes[];
+        primes = new boolean[MAX];
+        computePrimes(primes);
+        displayPrimes(primes);
+    }
+
+    public static void computePrimes(boolean primes[])
+    {
+        System.out.println("\nCOMPUTING PRIME NUMBERS");
+
+    }
+
+    public static void displayPrimes(boolean primes[])
+    {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
+        System.out.println();
+
+    }
+
+}
+
+
+
